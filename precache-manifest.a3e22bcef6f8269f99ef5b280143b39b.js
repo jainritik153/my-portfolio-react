d2ac6ace3440960e165ca6ab641b47d039350782
ritik <jainritik153@gmail.com>
2020-05-09 02:36:10 +0530

    Updates

---
+++ precache-manifest.a3e22bcef6f8269f99ef5b280143b39b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928cc331e8e947f5f6427dd39c3b235d",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "44a1bbab62daa29c4175",
+    "url": "/my-portfolio/static/css/main.54f432a5.chunk.css"
+  },
+  {
+    "revision": "494e845439d05235569c",
+    "url": "/my-portfolio/static/js/2.f5feee35.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/my-portfolio/static/js/2.f5feee35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44a1bbab62daa29c4175",
+    "url": "/my-portfolio/static/js/main.64ffa5af.chunk.js"
+  },
+  {
+    "revision": "a96c3fa99f5869bf6f69",
+    "url": "/my-portfolio/static/js/runtime-main.3511d9e1.js"
+  }
+]);
